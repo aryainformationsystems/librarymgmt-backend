5be9e5a8c7a7e8272650efaa15842634b0ed7a97
Chandrakant Singh <aryainformationsystems@gmail.com>
2021-10-25 20:30:40 +0530

    Added formatting to member model

--- models/member.js
+++ models/member.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const memberSchema = mongoose.Schema({
-name: String,
-address: String,
-phone: String,
-email: String,
-dateofbirth: Date,
+    name: String,
+    address: String,
+    phone: String,
+    email: String,
+    dateofbirth: Date,
 });
 
 module.exports = mongoose.model('member', memberSchema);
